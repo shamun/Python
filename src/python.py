acbd4b7b0b549bf983dbc4a11ec3f9000d8109ae
root <root@localhost>
2011-12-10 14:06:52 +0100

    Shots.

---
+++ src/python.py
@@ -0,0 +1,8 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="root"
+__date__ ="$Dec 10, 2011 2:05:29 PM$"
+
+if __name__ == "__main__":
+    print "Hello World"
